e1be7ccbe78300df7f3999e99788923368671b11
evilcops <hassan.iftikhar231999@gmail.com>
2022-03-16 23:36:49 +0500

    trainer done

--- routes/api/trainerDetails.js
+++ routes/api/trainerDetails.js
@@ -1,19 +1,30 @@
 var express = require('express')
 var router = express.Router()
 // // const { registerCustomer } = require('../../controllers/customerControl')
-// var { Verify } = require('../../middleware/coustomerDetails')
+var { Verify } = require('../../middleware/trainer')
+var { Hash } = require('../../middleware/coustomerDetails')
+var { Auth } = require('../../middleware/trainer')
 
 const {
-  //   getAllData,
-  //   getOneData,
-  //   updateData,
-  //   deleteData,
-  //   createData,
+  getAllData,
+  getOneData,
+  updateData,
+  deleteData,
+  loginUser,
   createData,
+  checkUser,
 } = require('../../controllers/trainer_Controller')
 
-router.route('/trainerregister').post(createData)
-// router.route('/trainer').get(getAllData)
-// router.route('/:trainerId').delete(deleteData).get(getOneData).patch(updateData)
+router.route('/trainerregister').post(Verify, Hash, createData)
+router.route('/trainer').get(getAllData)
+router
+  .route('/:trainerId')
+  .get(getOneData)
+  .patch(Verify, Hash, updateData)
+  .delete(deleteData)
 
+router.route('/login').post(loginUser)
+router.route('/log').post(Auth, checkUser)
 module.exports = router
+//
+//
