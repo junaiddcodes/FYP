a51dafd33d8567c80bf892c27c40228287587795
joonayd <junaidpakturk2@gmail.com>
2022-03-18 14:33:48 +0500

    frontend

--- frontend/src/Pages/Register/Login.jsx
+++ frontend/src/Pages/Register/Login.jsx
@@ -1,46 +1,67 @@
 import React, { useState } from "react";
 import "../../styles/pages.css";
-
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+const schema = yup.object().shape({
+  Email: yup.string().email().required(),
+  password: yup.string().min(8).max(20).required(),
+});
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [isSubmit, setIsSubmit] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
   return (
     <div className="page-container d-flex justify-content-center">
       <div className="outer-box-login d-flex flex-column justify-content-center align-items-center">
         <h2 className="text-center mb-3">Sign in</h2>
         <div className="inner-box-login d-flex flex-column justify-content-around ">
-          <form className="d-flex flex-column">
+          <form onSubmit={handleSubmit(submitForm)} className="d-flex flex-column">
             <div className="input-text d-flex flex-column">
-              <label for="lname">Email</label>
+              <label>Email</label>
               <input
                 type="email"
-                id=""
-                name=""
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
+                name="Email"
+                // value={email}
+                {...register("Email")}
+                // onChange={(e) => {
+                //   setEmail(e.target.value);
+                // }}
               />
-              <label for="lname">Password</label>
+              <p>{errors.Email?.message}</p>
+              <label>Password</label>
               <input
                 type="password"
-                id=""
-                name=""
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
+                name="password"
+                {...register("password")}
+                // value={password}
+                // onChange={(e) => {
+                //   setPassword(e.target.value);
+                // }}
               />
+              <p>{errors.password?.message}</p>
               <label for="lname">Sign in as</label>
             </div>
             <div className="d-flex mt-2 gender-radio justify-content-start">
-              <input name="gender" type="radio" value="Male" />
+              <input name="role" type="radio" value="user" />
               <h4>User</h4>
-              <input name="gender" type="radio" value="Female" />
+              <input name="role" type="radio" value="trainer" />
               <h4>Trainer</h4>
-              <input name="gender" type="radio" value="Both" />
+              <input name="role" type="radio" value="gym-owner" />
               <h4>Gym owner</h4>
             </div>
 
--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <div className="page-container d-flex justify-content-center">
-      <div className="outer-box d-flex flex-column justify-content-center align-items-center">
+      <div className="outer-box-register d-flex flex-column justify-content-center align-items-center">
         <h2 className="text-center mb-3">Signup as?</h2>
-        <div className="inner-box d-flex flex-column justify-content-around ">
+        <div className="inner-box-register d-flex flex-column justify-content-around ">
           <Link className="d-flex justify-content-center text-decoration-none" to="/register-user">
             <Button variant="primary">User</Button>
           </Link>
