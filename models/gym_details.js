261de37aa6602c742bddbbb36390a0d4f75b8588
Abdullah Bin Ikram <57711589+GoCodeAbdullah@users.noreply.github.com>
2022-04-28 22:50:57 +0500

    Gym Location Modify

--- models/gym_details.js
+++ models/gym_details.js
@@ -7,13 +7,13 @@ const Joi = require("joi");
 var gymDetailsSchema = mongoose.Schema({
   user_id: userSchema,
   listed: String, //Check the Gym is listed or not
-  location: String,
+  location: { city: String, address: String, state: String },
   gym_desc: String, //Store Gym Description
   gym_contact_no: String,
   gym_membership_price: Number,
   gender_facilitation: String,
   gym_photo: String,
-  cloudinary_id:String
+  cloudinary_id: String,
 });
 
 // Create Model For Gym in Gym_Details Table
@@ -29,13 +29,17 @@ function validateGym(data) {
       user_type: Joi.string().min(3).max(30).required(),
     },
     listed: Joi.string(),
-    location: Joi.string(),
+    location: {
+      city: Joi.string(),
+      address: Joi.string(),
+      state: Joi.string(),
+    },
     gym_desc: Joi.string(),
     gym_contact_no: Joi.string().min(5),
     gym_membership_price: Joi.number().positive(),
     gender_facilitation: Joi.string(),
     gym_photo: Joi.string(),
-    cloudinary_id: Joi.string()
+    cloudinary_id: Joi.string(),
   });
   return schema.validate(data);
 }
