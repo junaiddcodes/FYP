1b9fa2b3e5bda5e4f72aea13f4b63ae3acf6de77
joonayd <junaidpakturk2@gmail.com>
2022-06-02 12:34:44 +0500

    qwertyui

--- frontend/src/services/GenericService.js
+++ frontend/src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/api/";
+axios.defaults.baseURL = "http://finess-app-backend-test.herokuapp.com/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
   constructor() {}
