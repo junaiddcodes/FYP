871b95d97893ba93e374989a9804a22c8bc6221e
evilcops <hassan.iftikhar231999@gmail.com>
2022-06-04 15:23:26 +0500

    toast check

--- frontend/src/Pages/TrainerPanel/TrainerProfile.jsx
+++ frontend/src/Pages/TrainerPanel/TrainerProfile.jsx
@@ -235,7 +235,7 @@ const TrainerProfile = () => {
       // certificate_file: "",
       // trainer_photo: "",
     }
-    notify()
+
     trainerService
       .update_trainer(trainerProfileDetails, loggedInId)
       .then((data) => {
@@ -255,18 +255,18 @@ const TrainerProfile = () => {
     setSelectedValue(e.value)
   }
   return (
-    <div className="page-container-gym">
+    <div className='page-container-gym'>
       <TopBar />
       <SideMenuTrainer />
       <h2>Trainer Profile</h2>
       {isAsk ? (
-        <div className="gym-box mt-3 d-flex flex-column justify-content-start">
+        <div className='gym-box mt-3 d-flex flex-column justify-content-start'>
           <h4>
             There is no profile present. Click below to create a trainer
             profile:
           </h4>
           <Button
-            className="w-25 mt-4"
+            className='w-25 mt-4'
             onClick={() => {
               setIsTrainerForm(true)
               setIsProfilePicForm(false)
@@ -279,46 +279,46 @@ const TrainerProfile = () => {
       ) : isTrainerForm ? (
         <div>
           <Button
-            className="m-2"
+            className='m-2'
             onClick={() => {
               page_refresh()
             }}
           >
-            <i class="bx bx-arrow-back m-1"></i> Back
+            <i class='bx bx-arrow-back m-1'></i> Back
           </Button>
-          <div className="gym-box mt-3 d-flex flex-column align-items-left">
+          <div className='gym-box mt-3 d-flex flex-column align-items-left'>
             <form
               onSubmit={handleSubmitTrainerProfile(submitTrainerProfileForm)}
-              className="d-flex flex-column"
+              className='d-flex flex-column'
             >
-              <div className="input-text d-flex flex-column">
-                <div className="w-50 m-0">
+              <div className='input-text d-flex flex-column'>
+                <div className='w-50 m-0'>
                   <label>Your Name</label>
                   <input
-                    type="text"
-                    id=""
-                    name="full_name"
+                    type='text'
+                    id=''
+                    name='full_name'
                     {...controlTrainerProfile('full_name')}
                     defaultValue={getCustomer.user_id.full_name}
                   />
 
                   <p>{errorsTrainerProfile.full_name?.message}</p>
-                  <label for="fname">Select your exercise type</label>
-                  <FormControl className="m-3 w-100 dropdown-trainer">
+                  <label for='fname'>Select your exercise type</label>
+                  <FormControl className='m-3 w-100 dropdown-trainer'>
                     <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      name="exercise_type"
+                      labelId='demo-simple-select-label'
+                      id='demo-simple-select'
+                      name='exercise_type'
                       {...controlTrainerProfile('exercise_type')}
                       defaultValue={getCustomer.exercise_type}
                     >
-                      <MenuItem value="cardio">Cardio</MenuItem>
-                      <MenuItem value="gym">Gym</MenuItem>
-                      <MenuItem value="stretching">Stretching</MenuItem>
-                      <MenuItem value="boxing">boxing</MenuItem>
-                      <MenuItem value="aerobics">Aerobics</MenuItem>
-                      <MenuItem value="kickboxing">Kickboxing</MenuItem>
-                      <MenuItem value="swimming">Swimming</MenuItem>
+                      <MenuItem value='cardio'>Cardio</MenuItem>
+                      <MenuItem value='gym'>Gym</MenuItem>
+                      <MenuItem value='stretching'>Stretching</MenuItem>
+                      <MenuItem value='boxing'>boxing</MenuItem>
+                      <MenuItem value='aerobics'>Aerobics</MenuItem>
+                      <MenuItem value='kickboxing'>Kickboxing</MenuItem>
+                      <MenuItem value='swimming'>Swimming</MenuItem>
                     </Select>
                   </FormControl>
 
@@ -333,29 +333,29 @@ const TrainerProfile = () => {
                   {...controlTrainerProfile("exercise_type")}
                 /> */}
                   <p>{errorsTrainerProfile.exercise_type?.message}</p>
-                  <label for="fname">Enter Your certification</label>
-                  <FormControl className="m-3 w-100 dropdown-trainer">
+                  <label for='fname'>Enter Your certification</label>
+                  <FormControl className='m-3 w-100 dropdown-trainer'>
                     <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      name="qualification"
+                      labelId='demo-simple-select-label'
+                      id='demo-simple-select'
+                      name='qualification'
                       {...controlTrainerProfile('qualification')}
                       defaultValue={getCustomer.qualification}
                     >
-                      <MenuItem value="Master Fitness Instructor Course (MFIC)">
+                      <MenuItem value='Master Fitness Instructor Course (MFIC)'>
                         Master Fitness Instructor Course (MFIC)
                       </MenuItem>
-                      <MenuItem value="                        Unarmed Combat & Bayonet Fighting Course (UCBC)">
+                      <MenuItem value='                        Unarmed Combat & Bayonet Fighting Course (UCBC)'>
                         Unarmed Combat & Bayonet Fighting Course (UCBC)
                       </MenuItem>
-                      <MenuItem value="                        Advance Unarmed Combat & Bayonet Fighting Course (Adv UCBC)">
+                      <MenuItem value='                        Advance Unarmed Combat & Bayonet Fighting Course (Adv UCBC)'>
                         Advance Unarmed Combat & Bayonet Fighting Course (Adv
                         UCBC)
                       </MenuItem>
-                      <MenuItem value="                        Sports Coaching Courses army school of training">
+                      <MenuItem value='                        Sports Coaching Courses army school of training'>
                         Sports Coaching Courses army school of training
                       </MenuItem>
-                      <MenuItem value="Water Sports Course (WSC)">
+                      <MenuItem value='Water Sports Course (WSC)'>
                         Water Sports Course (WSC)
                       </MenuItem>
                     </Select>
@@ -365,21 +365,21 @@ const TrainerProfile = () => {
 
                 <label>Enter the company you are currently working at</label>
                 <input
-                  type="text"
-                  id=""
-                  name="company_name"
+                  type='text'
+                  id=''
+                  name='company_name'
                   {...controlTrainerProfile('company_name')}
                   defaultValue={getCustomer.company_name}
                 />
 
                 <p>{errorsTrainerProfile.company_name?.message}</p>
-                <label for="lname">
+                <label for='lname'>
                   Enter your designation in the current company{' '}
                 </label>
                 <input
-                  type="text"
-                  id=""
-                  name="designation"
+                  type='text'
+                  id=''
+                  name='designation'
                   {...controlTrainerProfile('designation')}
                   defaultValue={getCustomer.designation}
                 />
@@ -398,11 +398,11 @@ const TrainerProfile = () => {
                 {/* <label for="lname">Your gender</label> */}
               </div>
 
-              <label for="lname">Your details</label>
+              <label for='lname'>Your details</label>
 
               <textarea
-                className="text-field mt-2"
-                name="trainer_desc"
+                className='text-field mt-2'
+                name='trainer_desc'
                 {...controlTrainerProfile('trainer_desc')}
                 defaultValue={getCustomer.trainer_desc}
               />
@@ -411,13 +411,13 @@ const TrainerProfile = () => {
               {/* <label for="lname">Upload certificate file</label>
             <p className="general-p">Please upload your profile picture</p>
           <input type="file" /> */}
-              <p className="general-p mt-5">
+              <p className='general-p mt-5'>
                 Submit Profile to the Admin. Admin will review your profile and
                 Approve it:
               </p>
               <Button
-                type="submit"
-                className="w-25 mt-3"
+                type='submit'
+                className='w-25 mt-3'
                 // onClick={() => {
                 //   setIsProfile(true);
                 // }}
@@ -431,35 +431,35 @@ const TrainerProfile = () => {
       ) : isProfilePicForm ? (
         <div>
           <Button
-            className="m-2"
+            className='m-2'
             onClick={() => {
               setIsProfile(true)
               setIsProfilePicForm(false)
             }}
           >
-            <i class="bx bx-arrow-back m-1"></i> Back
+            <i class='bx bx-arrow-back m-1'></i> Back
           </Button>
-          <p className="general-p">Please upload your profile picture</p>
-          <div className="upload-photo-card">
+          <p className='general-p'>Please upload your profile picture</p>
+          <div className='upload-photo-card'>
             <TransformWrapper>
               <TransformComponent>
-                <img className="preview" src={previewImage} alt="" />
+                <img className='preview' src={previewImage} alt='' />
               </TransformComponent>
             </TransformWrapper>
           </div>
-          <form onSubmit={changeOnClick} encType="multipart/form-data">
-            <div className="upload-form">
+          <form onSubmit={changeOnClick} encType='multipart/form-data'>
+            <div className='upload-form'>
               <input
                 style={{ marginTop: '1rem' }}
-                accept="image/*"
-                type="file"
-                filename="trainer"
+                accept='image/*'
+                type='file'
+                filename='trainer'
                 onChange={onChangeFile}
               />
               <button
                 style={{ marginTop: '1rem', marginBottom: '1rem' }}
-                className="btn btn-primary w-25"
-                type="submit"
+                className='btn btn-primary w-25'
+                type='submit'
               >
                 submit
               </button>
@@ -467,25 +467,25 @@ const TrainerProfile = () => {
           </form>
         </div>
       ) : isProfile ? (
-        <div className="trainer-desc mt-3 d-flex flex-column">
-          <div className="d-flex ">
-            <div className="d-flex w-75 justify-content-between">
-              <div className="trainer-photo d-flex">
+        <div className='trainer-desc mt-3 d-flex flex-column'>
+          <div className='d-flex '>
+            <div className='d-flex w-75 justify-content-between'>
+              <div className='trainer-photo d-flex'>
                 <img
-                  clasName="trainer-photo"
+                  clasName='trainer-photo'
                   src={getCustomer.trainer_photo}
-                  alt=""
+                  alt=''
                 />
-                <div className="d-flex mt-5 flex-column">
+                <div className='d-flex mt-5 flex-column'>
                   <h4>Name: {getCustomer.user_id.full_name}</h4>
                   <h4>Age: {trainerAge}</h4>
                   <h4>Gender: {getCustomer.gender}</h4>
                   <h4>Status: {getCustomer.listed}</h4>
                 </div>
               </div>
-              <div className="trainer-btn d-flex flex-column">
+              <div className='trainer-btn d-flex flex-column'>
                 <Button
-                  className="mt-5"
+                  className='mt-5'
                   onClick={() => {
                     setIsTrainerForm(true)
                     setIsProfile(false)
@@ -494,7 +494,7 @@ const TrainerProfile = () => {
                   Edit
                 </Button>
                 <Button
-                  className="mt-5"
+                  className='mt-5'
                   onClick={() => {
                     setIsProfilePicForm(true)
                     setIsProfile(false)
@@ -504,14 +504,14 @@ const TrainerProfile = () => {
                   Edit profile picture
                 </Button>
                 <Button
-                  className="mt-5"
+                  className='mt-5'
                   onClick={() => {
                     setConfirmDelete(true)
                   }}
                 >
                   Delete
                 </Button>
-                <div className="modal-container">
+                <div className='modal-container'>
                   <Modal
                     style={{
                       overlay: {
@@ -538,27 +538,27 @@ const TrainerProfile = () => {
                         padding: '20px',
                       },
                     }}
-                    className="w-50 d-flex flex-column justify-content-around align-items-center add-food-modal"
+                    className='w-50 d-flex flex-column justify-content-around align-items-center add-food-modal'
                     isOpen={confirmDelete}
                     onRequestClose={() => {
                       setConfirmDelete(false)
                     }}
                   >
-                    <div className="modal-inner w-75 d-flex flex-column">
+                    <div className='modal-inner w-75 d-flex flex-column'>
                       <a
                         onClick={() => {
                           setConfirmDelete(false)
                         }}
                       >
-                        <i class="bx bx-x"></i>
+                        <i class='bx bx-x'></i>
                       </a>
                       <h3>Are you sure you want to delete the profile?</h3>
                       <p>Select yes to delete the profile</p>
                     </div>
-                    <div className="d-flex">
+                    <div className='d-flex'>
                       <Button
-                        className="btn-dark m-3"
-                        type="submit "
+                        className='btn-dark m-3'
+                        type='submit '
                         onClick={() => {
                           trainerProfileDetails = {
                             ...trainerProfileDetails,
@@ -571,7 +571,7 @@ const TrainerProfile = () => {
                             certificate_file: '',
                             trainer_photo: '',
                           }
-                          notify()
+
                           trainerService
                             .update_trainer(trainerProfileDetails, loggedInId)
                             .then((data) => {
@@ -588,8 +588,8 @@ const TrainerProfile = () => {
                         Yes
                       </Button>
                       <Button
-                        className="m-3"
-                        type="submit"
+                        className='m-3'
+                        type='submit'
                         onClick={() => {
                           setConfirmDelete(false)
                         }}
@@ -602,7 +602,7 @@ const TrainerProfile = () => {
               </div>
             </div>
           </div>
-          <div className="m-4 d-flex flex-column">
+          <div className='m-4 d-flex flex-column'>
             <h4>Certification: {getCustomer.qualification}</h4>
             <h4>Exercise Type: {getCustomer.exercise_type}</h4>
             <h4>Company Name: {getCustomer.company_name}</h4>
